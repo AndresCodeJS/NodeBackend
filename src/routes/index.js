0b5d7e01fef0a21e376241aaf7dac39ef2e20158
Andres Gomez <andres2661991@gmail.com>
2023-01-12 11:14:28 -0300

    features v5

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import Router from 'express'
 import User from './User.js'
 import Order from './Order.js'
 import Product from './Product.js'
+import Entry from './Entry.js'
 
 const router = Router();
 
 router.use("/user",User)
 router.use("/order",Order)
 router.use("/product",Product)
+router.use("/entry",Entry)
 
 export default router
