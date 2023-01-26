77d2bbdc7d338504c949f0b740f4ad095655e3eb
Andres Gomez <andres2661991@gmail.com>
2023-01-26 18:42:19 -0300

    restaurant app

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import User from './User.js'
 import Order from './Order.js'
 import Product from './Product.js'
 import Entry from './Entry.js'
+import Dish from './Dish.js'
 
 const router = Router();
 
@@ -10,5 +11,8 @@ router.use("/user",User)
 router.use("/order",Order)
 router.use("/product",Product)
 router.use("/entry",Entry)
+
+//Restaurant App
+router.use("/dish",Dish)
 
 export default router
